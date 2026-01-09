021330b6c77a0a565556a336d1c90c319c723944
CHoU <handsomecheung@gmail.com>
2026-01-09 11:12:28 +0900

    setup longhorn recurring jobs

--- koishi/kubernetes/longhorn/deploy.sh
+++ koishi/kubernetes/longhorn/deploy.sh
@@ -5,3 +5,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 kubectl apply -f app.install.yaml
 my-k8s-deploy --file=app.ingress.yaml
 my-k8s-deploy --file=app.volumes.yaml
+my-k8s-deploy --file=app.recurring.yaml
