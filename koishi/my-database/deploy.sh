8289cb69b8ba6ded851d8269fbb4afaec2d99cf6
CHoU <handsomecheung@gmail.com>
2026-01-15 12:18:30 +0900

    move database applications to polyphony

---
+++ koishi/my-database/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+my-k8s-deploy --file=app.namespace.yaml
+my-k8s-deploy --file=app.redis.yaml
+my-k8s-deploy --file=app.postgres.yaml
+my-k8s-deploy --file=app.dbgate.yaml
