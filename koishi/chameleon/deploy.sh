8e9f4aa90211fa80d69fceefd734125fc2d4e036
CHoU <handsomecheung@gmail.com>
2026-01-27 10:23:20 +0900

    minor changes

--- koishi/chameleon/deploy.sh
+++ koishi/chameleon/deploy.sh
@@ -5,3 +5,18 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 my-k8s-deploy --file=k8s.namespace.yaml
 my-k8s-deploy --file=k8s.volumes.yaml
+
+./squid/deploy.sh
+./gollum/deploy.sh
+./deactivate/deploy.sh
+./pikvm-controller/deploy.sh
+
+./glinux/deploy.sh
+
+./gotty/deploy.sh
+./mbtty/deploy.sh
+
+./mbvnc/deploy.sh
+./novnc/deploy.sh
+
+./homego/deploy.sh
