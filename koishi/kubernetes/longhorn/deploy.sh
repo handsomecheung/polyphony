13819d5a56f7858304c851f136147cf2c87aac0b
CHoU <handsomecheung@gmail.com>
2026-01-31 19:54:32 +0900

    upgrade longhorn to v1.11.0 from v1.10.1

--- koishi/kubernetes/longhorn/deploy.sh
+++ koishi/kubernetes/longhorn/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-kubectl apply -f app.install.yaml
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.11.0/deploy/longhorn.yaml
 my-k8s-deploy --file=app.ingress.yaml
 my-k8s-deploy --file=app.volumes.yaml
 my-k8s-deploy --file=app.recurring.yaml
