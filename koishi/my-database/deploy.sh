8852120e624337fd10166470738ee9329e3a139c
CHoU <handsomecheung@gmail.com>
2026-02-09 18:37:52 +0900

    database: separate PostgreSQL into dev and prod instances and update dependent services

--- koishi/my-database/deploy.sh
+++ koishi/my-database/deploy.sh
@@ -4,6 +4,11 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 my-k8s-deploy --file=app.namespace.yaml
+
 my-k8s-deploy --file=app.redis.yaml
-my-k8s-deploy --file=app.postgres.yaml
+
+my-k8s-deploy --file=app.postgres.dev.yaml
+my-k8s-deploy --file=app.postgres.prod.yaml
+
 my-k8s-deploy --file=app.dbgate.yaml
+my-k8s-deploy --file=app.dbgate.cc.yaml
