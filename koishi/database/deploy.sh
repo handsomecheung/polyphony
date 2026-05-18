7a30e38379c4a978f46fc8844e07677e2a9f8d41
CHoU <handsomecheung@gmail.com>
2026-05-18 22:34:24 +0900

    Refactor PostgreSQL deployment structure and migrate to subdirectories

--- koishi/database/deploy.sh
+++ koishi/database/deploy.sh
@@ -8,7 +8,8 @@ my-k8s-deploy --file=k8s.app.namespace.yaml
 my-k8s-deploy --file=k8s.app.redis.prod.yaml
 my-k8s-deploy --file=k8s.app.redis.dev.yaml
 
-my-k8s-deploy --file=k8s.app.postgres.dev.yaml
-my-k8s-deploy --file=k8s.app.postgres.prod.yaml
+./postgres.dev/deploy.sh
+
+./postgres.prod/deploy.sh
 
 ./tools/deploy.sh
