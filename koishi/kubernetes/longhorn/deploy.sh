eaa8c2d4451ad9e7ec022d1e740e3f6dfaa0eef9
CHoU <handsomecheung@gmail.com>
2026-05-18 16:56:06 +0900

    Remove Longhorn storage system and associated configurations

--- koishi/kubernetes/longhorn/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.11.0/deploy/longhorn.yaml
-my-k8s-deploy --file=app.ingress.yaml
-my-k8s-deploy --file=app.volumes.yaml
-my-k8s-deploy --file=app.recurring.yaml
