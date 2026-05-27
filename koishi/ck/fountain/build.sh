add63f883a430b00df0d94ec5b1917fd81a93324
CHoU <handsomecheung@gmail.com>
2026-05-27 18:29:17 +0900

    refactor(ck/fountain): update namespace to ck-prod and reduce replicas

--- koishi/ck/fountain/build.sh
+++ koishi/ck/fountain/build.sh
@@ -2,4 +2,4 @@
 set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-my-k8s-build-image "cloudpublic/ck/fountain/scanner:latest" ck-fountain-prod scanner
+my-k8s-build-image "cloudpublic/ck/fountain/scanner:latest" ck-prod scanner
