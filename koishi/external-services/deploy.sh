b33b118b7967024a02e41f0d4a6eafbc2ef2d33f
CHoU <handsomecheung@gmail.com>
2026-06-17 12:19:25 +0900

    chore(docker-compose): update nippon local config

--- koishi/external-services/deploy.sh
+++ koishi/external-services/deploy.sh
@@ -6,7 +6,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 my-k8s-deploy --file=k8s.cockpit.yaml
 my-k8s-deploy --file=k8s.vm.yaml
 my-k8s-deploy --file=k8s.router.yaml
-my-k8s-deploy --file=k8s.stable-diffusion.yaml
 my-k8s-deploy --file=k8s.shutdown.yaml
 my-k8s-deploy --file=k8s.wakeup.yaml
 my-k8s-deploy --file=k8s.pikvm.yaml
