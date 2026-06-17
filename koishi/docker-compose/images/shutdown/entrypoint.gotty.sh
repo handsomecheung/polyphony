7cbd46a3d71d44f2b000a68260876f9923c61432
CHoU <handsomecheung@gmail.com>
2026-06-17 12:23:29 +0900

    chore(docker-compose): increase drain timeout and handle failure in shutdown entrypoint

--- koishi/docker-compose/images/shutdown/entrypoint.gotty.sh
+++ koishi/docker-compose/images/shutdown/entrypoint.gotty.sh
@@ -10,8 +10,8 @@ if [ "${auto}" == "true" ]; then
 	sleep 1800
 
 	now=$(date +"%H:%M")
-    start_time="00:00"
-    end_time="06:00"
+	start_time="00:00"
+	end_time="06:00"
 
 	if [[ "$now" < "$start_time" ]] || [[ "$now" > "$end_time" ]]; then
 		echo "The time ${now} is not between ${start_time} and ${end_time}. Shutdown aborted."
@@ -28,17 +28,17 @@ fi
 if ! tmux has-session -t "${session}" 2>/dev/null; then
 	echo "Session does not exist."
 
-    if [ ! "${auto}" == "true" ]; then
-	    echo "Really want to shutdown? (y/N)"
-	    read -r -n 1 -s answer
-	    if [ "${answer}" != "Y" ] && [ "${answer}" != "y" ]; then
-	    	echo "Shutdown aborted."
-	    	exit 0
-	    fi
-    fi
+	if [ ! "${auto}" == "true" ]; then
+		echo "Really want to shutdown? (y/N)"
+		read -r -n 1 -s answer
+		if [ "${answer}" != "Y" ] && [ "${answer}" != "y" ]; then
+			echo "Shutdown aborted."
+			exit 0
+		fi
+	fi
 
 	echo "Shutting down. node: ${node} ..."
-	tmux new -d -A -s "${session}" bash -c "timeout 120 kubectl drain --delete-emptydir-data --ignore-daemonsets ${node} ; systemctl poweroff"
+	tmux new -d -A -s "${session}" bash -c "timeout 600 kubectl drain --delete-emptydir-data --ignore-daemonsets ${node} && systemctl poweroff || { echo 'ERROR: kubectl drain failed. Shutdown aborted!'; tail -f /dev/null; }"
 fi
 
 tmux attach-session -t "${session}" -r
