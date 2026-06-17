bac50a2e7d94809ac8c16064bbfa7fa9537c96ba
CHoU <handsomecheung@gmail.com>
2026-06-17 12:22:29 +0900

    chore(external-services): remove yx service and update deploy script

--- koishi/external-services/deploy.sh
+++ koishi/external-services/deploy.sh
@@ -9,5 +9,3 @@ my-k8s-deploy --file=k8s.router.yaml
 my-k8s-deploy --file=k8s.shutdown.yaml
 my-k8s-deploy --file=k8s.wakeup.yaml
 my-k8s-deploy --file=k8s.pikvm.yaml
-
-bash yx/deploy.sh
--- koishi/external-services/yx/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-my-k8s-deploy --file=k8s.namespace.yaml
-my-k8s-deploy --file=k8s.localdev.yaml
-my-k8s-deploy --file=k8s.localprod.yaml
