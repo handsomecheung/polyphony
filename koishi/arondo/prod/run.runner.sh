07d9648d91956515708be49c14ceaea25fa2f60a
CHoU <handsomecheung@gmail.com>
2026-07-02 12:59:14 +0900

    chore(arondo/prod): update volumes, Dockerfile, and runner configuration
    - Dockerfile: Copy register-env.ts
    - Kubernetes configuration: Use config volume and mount to /home/node/.arondo
    - Runner script: Refactor variable usage and comment out tailing log

--- koishi/arondo/prod/run.runner.sh
+++ koishi/arondo/prod/run.runner.sh
@@ -4,7 +4,11 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/"
 source .env
 
-pids=$(pgrep -f "runner --server wss://${ARONDO_HOST_SERVER_PROD}/runner --name devbox-nur" || true)
+runner=devbox-nur
+file_bin=/tmp/arondo.prod.runner
+file_log=/tmp/arondo.prod.runner.log
+
+pids=$(pgrep -f "runner --server wss://${ARONDO_HOST_SERVER_PROD}/runner --name ${runner}" || true)
 if [ -n "$pids" ]; then
   echo "Stopping previous runner processes: $pids"
   kill $pids || true
@@ -15,10 +19,7 @@ if [ -n "$pids" ]; then
   done
 fi
 
-file_bin=/tmp/arondo.prod.runner
-file_log=/tmp/arondo.prod.runner.log
-
 wget https://github.com/handsomecheung/Arondo/releases/latest/download/runner-linux-amd64 -O "${file_bin}"
 chmod +x "${file_bin}"
-nohup bash -c "/tmp/arondo.prod.runner --server wss://${ARONDO_HOST_SERVER_PROD}/runner --name devbox-nur" >"${file_log}" 2>&1 &
-tail -f "${file_log}"
+nohup bash -c "/tmp/arondo.prod.runner --server wss://${ARONDO_HOST_SERVER_PROD}/runner --name ${runner}" >"${file_log}" 2>&1 &
+# tail -f "${file_log}"
