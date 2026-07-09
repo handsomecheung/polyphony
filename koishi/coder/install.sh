f791bc149b9c3256017ff45f4c7ae64218fcd7c8
CHoU <handsomecheung@gmail.com>
2026-07-09 22:48:36 +0900

    chore(coder): remove sablier middleware and scale-to-zero config
    Drop the sablier middleware deployment and its wiring in install/update
    scripts, and revert coder to running a normal always-on replica.

--- koishi/coder/install.sh
+++ koishi/coder/install.sh
@@ -3,7 +3,6 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 my-k8s-deploy --meta=pullsecret-cloudprivate --namespace=default
-my-k8s-deploy --file=k8s.middleware.yaml
 
 dir=$(mktemp -d -t k8s-deploy-coder-XXXXXX)
 cp values.yaml "${dir}"
